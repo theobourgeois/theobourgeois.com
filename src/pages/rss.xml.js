98ec5b4030346a11297d712bc94de5fa69d42488
theobourgeois <theoobourgeois@gmail.com>
2024-06-09 16:53:56 -0300

    fix rss.xml???

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,6 +5,14 @@ export async function GET(context) {
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site
+		site: context.site,
+		items: [
+			{
+				title: 'Item 1',
+				description: 'Description for Item 1',
+				pubDate: new Date(),
+				link: 'https://example.com/item1'
+		},	
+		]
 	})
 }
